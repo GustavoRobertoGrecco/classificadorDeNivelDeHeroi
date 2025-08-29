f00a4c4984d81fa20443b0a25fdea6f6b3d8d570
GustavoRobertoGrecco <117321912+GustavoRobertoGrecco@users.noreply.github.com>
2025-08-29 11:02:17 +0000

    Primeiro teste

---
+++ index.js
@@ -0,0 +1,12 @@
+// declarando as variáveis
+let nomeDoHeroi = "DIO Student"
+let xpDoHeroi = "999"
+
+
+//Estrutura de condicionais do nível do herói
+
+switch (xpDoHeroi){
+    case: <1000
+    console.log("O Herói de nome " + nomeDoHeroi + "está no nível de Ferro")
+    break
+}
